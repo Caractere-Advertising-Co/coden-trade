eb5b9c6dab675e1a7a71ec6547661247dbae0038
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-05-08 13:59:57 +0200

    catalogues

--- catalogues.php
+++ catalogues.php
@@ -3,7 +3,10 @@
 
 $bg_header = get_field('bg_header');
 
-if($bg_header):
+if(!$bg_header):
+    $bg_url = get_template_directory_uri(  ).'/assets/img/bg-default.jpg';
+else :
+    $bg_header = get_field('bg_header');
     $bg_url = $bg_header['url'];
 endif;
 
