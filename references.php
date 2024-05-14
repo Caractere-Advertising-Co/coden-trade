f3f36b9001722809a4135427187bb99ddaa426bb
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-05-14 10:50:19 +0200

    ajout h1 page reférences

--- references.php
+++ references.php
@@ -12,7 +12,7 @@ get_header();?>
     <div class="container">
         <div class="colg">
             <?php if($title):
-            echo $title;
+            echo '<h1>'.$title.'</h1>';
         endif;?>
         </div>
         <div class="cold">
