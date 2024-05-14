f3a0a0464fc6351f3c41391a30a4732d0877dfe2
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-05-14 11:46:08 +0200

    Modif pag conseils

--- conseils.php
+++ conseils.php
@@ -23,40 +23,13 @@ $intro = get_field('introduction');
 
     <div class="container">
         <div class="grid_articles">
-            <?php 
-            $args = array(
-                'post_type' => 'post',
-                'category__not_in' => 60,
-                'posts_per_page'=> 9,
-                'post_statut' => 'publish'
-            );
 
-            $query = new WP_Query( $args );
-
-            if($query->have_posts()):
-                while($query->have_posts()): $query->the_post();?>
-            
-                <div class="card_article from-bottom">
-                    <a href="<?php the_permalink();?>" class="red">
-                        <div class="miniature">
-                            <img src="<?php if(has_post_thumbnail()) : the_post_thumbnail_url(); endif;?>"/>
-                        </div>
-
-                        <h4><?php the_date();?></h4>
-                        <h3><?php the_title();?></h3>
-
-                        <a href="">Découvrir</a>
-                    </a>
-                </div>
-            <?php
+            <?php if(have_rows('pdfFiles')):
+                while(have_rows('pdfFiles')): the_row();
+                    $pdf = get_sub_field('pdf');
+                    echo do_shortcode($pdf);
                 endwhile;
-            endif;
-            
-            wp_reset_postdata();?>
-        </div>
-
-        <div class="load-more-container">
-            <a id="load-more-posts" class="cta" href="!#">Voir plus</a>
+            endif;?>
         </div>
     </div>
 </section>
