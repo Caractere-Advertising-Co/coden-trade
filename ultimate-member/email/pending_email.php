d72f0640dba50957434d9b2c78c139b255e00820
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-07-16 13:55:16 +0200

    modification pending message

--- ultimate-member/email/pending_email.php
+++ ultimate-member/email/pending_email.php
@@ -1,25 +1,28 @@
-<div style="width:700px;">
+<div style="width:500px;text-align:center;font-family:'montserrat',sans-serif;">
     <!-- HEADER -->
-    <div style="background:red;height:30px;">
-        <div style="margin:auto;width:2px;background:#fff;"></div>
+    <div style="background:#a42e2b;height:30px;">
+        <div style="margin:auto;width:2px;background:#fff;display:block;height:30px;"></div>
     </div>
 
     <!-- CONTENT -->
+    <img src="https://coden.caractere-advertising.be/wp-content/uploads/2024/05/Calque-2.svg" alt="Logo-coden"/>
 
-    <h1 style="text-transform:uppercase;font-size:2rem;color:#fff;">
+    <h1 style="text-transform:uppercase;font-size:2rem;color:#fff;font-family:'Montserrat',sans-serif;">
         validation<br> de votre compte
     </h1>
 
-    <p style="padding: 30px 0;font-size: 24px;text-align: center;line-height: 40px">
+    <p style="padding: 30px 0;font-size: 1.2rem;text-align: center;line-height: 2rem;">
         Merci pour votre inscription ! <br />
-        Votre compte doit être vérifié manuellement avant toute utilisation.
+        Votre compte doit être vérifié<br> 
+        manuellement avant toute <br>
+        utilisation.
     </p>
 
-    <a href="http://coden.caractere-advertising.be" style="color:#fff; text-transform: uppercase;background:red;width: fit-content; padding: 5px 20px;">Visiter la boutique</a>
+    <a href="http://coden.caractere-advertising.be" style="color:#fff;text-transform: uppercase;background: #a42e2b;width: fit-content;padding: 10px 20px;margin: auto;display: block;text-decoration: none;letter-spacing: 4px;font-size: 1.2rem;">Visiter la boutique</a>
 
-    <p style="text-align: center; font-style:italic; font-size: .8rem;">Veuillez nous accorder un certain temps pour traiter votre demande et examiner vos informations.</p>
+    <p style="font-style:italic; font-size: .8rem;">Veuillez nous accorder un certain temps pour traiter votre<br> demande et examiner vos informations.</p>
 
-
+    <div style="display:block; width: 50px; background:#a42e2b;height:2px;margin:auto;"></div>
     <!-- FOOTER -->
     <div style="color: #999;padding: 20px 30px; text-align:center;">        
         <p>Merci!<br>
