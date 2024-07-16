468aa76de7ab13209d3b6c2368904f8872f83d82
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-07-16 14:09:16 +0200

    Correction mail nouveau compte validation

--- ultimate-member/email/pending_email.php
+++ ultimate-member/email/pending_email.php
@@ -4,10 +4,12 @@
         <div style="margin:auto;width:2px;background:#fff;display:block;height:30px;"></div>
     </div>
 
-    <!-- CONTENT -->
-    <img src="https://coden.caractere-advertising.be/wp-content/uploads/2024/05/Calque-2.svg" alt="Logo-coden"/>
+    <div style="margin:auto;width:2px;background:#a42e2b;display:block;height:10px;"></div>
 
-    <h1 style="text-transform:uppercase;font-size:2rem;color:#fff;font-family:'Montserrat',sans-serif;">
+    <!-- CONTENT -->
+    <img src="https://coden.caractere-advertising.be/wp-content/uploads/2024/05/Calque-2.svg" width="250" style="width:250px;" alt="Logo-coden"/>
+
+    <h1 style="text-transform:uppercase;font-size:2rem;color:#000;font-family:'Montserrat',sans-serif;font-weight:200">
         validation<br> de votre compte
     </h1>
 
@@ -18,15 +20,15 @@
         utilisation.
     </p>
 
-    <a href="http://coden.caractere-advertising.be" style="color:#fff;text-transform: uppercase;background: #a42e2b;width: fit-content;padding: 10px 20px;margin: auto;display: block;text-decoration: none;letter-spacing: 4px;font-size: 1.2rem;">Visiter la boutique</a>
+    <a href="http://coden.caractere-advertising.be" style="color:#fff;text-transform: uppercase;background: #a42e2b;width: fit-content;padding: 10px 20px;margin: auto;margin-bottom:20px;display: block;text-decoration: none;letter-spacing: 4px;font-size: 1.2rem;">Visiter la boutique</a>
 
-    <p style="font-style:italic; font-size: .8rem;">Veuillez nous accorder un certain temps pour traiter votre<br> demande et examiner vos informations.</p>
+    <p style="font-style:italic; font-size: .8rem;margin-bottom:30px;">Veuillez nous accorder un certain temps pour traiter votre<br> demande et examiner vos informations.</p>
 
     <div style="display:block; width: 50px; background:#a42e2b;height:2px;margin:auto;"></div>
     <!-- FOOTER -->
-    <div style="color: #999;padding: 20px 30px; text-align:center;">        
+    <div style="padding: 20px 30px; text-align:center;">        
         <p>Merci!<br>
-        L'équipe <a style="color: #3ba1da;text-decoration: none" href="{site_url}">{site_name}</a></p>     
+        L'équipe <a style="color:#a42e2b;text-decoration: none" href="{site_url}">{site_name}</a></p>     
     </div>
     <div style="margin:auto;width:2px;background:#a42e2b;display:block;height:30px;"></div>
 </div>
