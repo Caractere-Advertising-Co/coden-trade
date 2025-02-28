9c9db0087b330b9112c240cded9dcb2bd30f82cc
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-02-28 09:47:09 +0100

    upsell

--- woocommerce/single-product/up-sells.php
+++ woocommerce/single-product/up-sells.php
@@ -72,22 +72,7 @@ if ( $upsells ) : ?>
 								<div class="bubble <?php echo $tagClass;?>">
 									<p><?php foreach($tags as $t): echo $t->name; endforeach;?></p>
 								</div>
-							<?php endif;
-							/**
-							 * Hook: woocommerce_before_shop_loop_item.
-							 *
-							 * @hooked woocommerce_template_loop_product_link_open - 10
-							 */
-							do_action( 'woocommerce_before_shop_loop_item' );
-
-							/**
-							 * Hook: woocommerce_before_shop_loop_item_title.
-							 *
-							 * @hooked woocommerce_show_product_loop_sale_flash - 10
-							 * @hooked woocommerce_template_loop_product_thumbnail - 10
-							 */
-							do_action( 'woocommerce_before_shop_loop_item_title' );
-							
+							<?php endif; 
 							?>
 
 							<div class="content-product">
