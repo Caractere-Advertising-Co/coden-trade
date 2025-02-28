3c1e54b5530b38d340dd1840836fbff7779f9fe1
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-02-28 09:43:40 +0100

    custom upsell

--- woocommerce/single-product/up-sells.php
+++ woocommerce/single-product/up-sells.php
@@ -37,8 +37,85 @@ if ( $upsells ) : ?>
 
 				setup_postdata( $GLOBALS['post'] =& $post_object ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited, Squiz.PHP.DisallowMultipleAssignments.Found
 
-				wc_get_template_part( 'content', 'product' );
-				?>
+					$pid = $upsell->id;
+					$cat = get_the_terms( $pid, 'product_cat' );
+					$tags = get_the_terms( $pid, 'product_tag' );
+
+					if($tags):
+						foreach($tags as $t):
+							switch($t->name){
+								case 'Promo':
+									$tagClass = '-white';
+									break;
+								case 'New':
+									$tagClass = '-black';
+									break;
+								default:
+									$tagClass = '';
+									break;
+							}
+						endforeach;
+					endif;
+
+					// Ensure visibility.
+					if ( empty( $upsell ) || ! $upsell->is_visible() ) {
+						return;
+					}
+
+					?>
+									
+					<li <?php wc_product_class( 'card_product', $upsell ); ?>><?php
+
+						if($tags): ?>
+							<div class="bubble <?php echo $tagClass;?>">
+								<p><?php foreach($tags as $t): echo $t->name; endforeach;?></p>
+							</div>
+						<?php endif;
+						/**
+						 * Hook: woocommerce_before_shop_loop_item.
+						 *
+						 * @hooked woocommerce_template_loop_product_link_open - 10
+						 */
+						do_action( 'woocommerce_before_shop_loop_item' );
+
+						/**
+						 * Hook: woocommerce_before_shop_loop_item_title.
+						 *
+						 * @hooked woocommerce_show_product_loop_sale_flash - 10
+						 * @hooked woocommerce_template_loop_product_thumbnail - 10
+						 */
+						do_action( 'woocommerce_before_shop_loop_item_title' );
+						
+						?>
+
+						<div class="content-product">
+						<?php 
+						/**
+						 * Hook: woocommerce_shop_loop_item_title.
+						 *
+						 * @hooked woocommerce_template_loop_product_title - 10
+						 */
+
+						echo '<span class="title"><h3>'. get_the_title($pid).'</h3></span>';
+
+						/**
+						 * Hook: woocommerce_after_shop_loop_item_title.
+						 *
+						 * @hooked woocommerce_template_loop_rating - 5
+						 * @hooked woocommerce_template_loop_price - 10
+						 */
+						do_action( 'woocommerce_after_shop_loop_item_title' );
+
+						/**
+						 * Hook: woocommerce_after_shop_loop_item.
+						 *
+						 * @hooked woocommerce_template_loop_product_link_close - 5
+						 * @hooked woocommerce_template_loop_add_to_cart - 10
+						 */
+						do_action( 'woocommerce_after_shop_loop_item' );
+						?>
+						</div>
+					</li>
 			<?php endforeach; ?>
 
 		<?php woocommerce_product_loop_end(); ?>
