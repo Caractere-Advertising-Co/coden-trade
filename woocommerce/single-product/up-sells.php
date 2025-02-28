d589da51c981a710ca3a4a1ed474a4820f95b74f
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-02-28 10:00:20 +0100

    remove action

--- woocommerce/single-product/up-sells.php
+++ woocommerce/single-product/up-sells.php
@@ -80,6 +80,7 @@ if ( $upsells ) : ?>
 								 * @hooked woocommerce_template_loop_product_thumbnail - 10
 								 */
 								do_action( 'woocommerce_before_shop_loop_item_title' );
+								remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10 );
 								
 								?>
 
