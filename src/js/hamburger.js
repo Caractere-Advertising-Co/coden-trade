070bd34dd8e1113ff85c4d2798b21a2345758b1a
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-02-28 11:14:59 +0100

    classlisttoggle

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
   var hamburger = document.querySelector(".hamburger-menu");
   var menu = document.querySelector(".megamenu");
   var header = document.querySelector(".navigation");
-  var body = $('body');
+  var body = $('body'); // jQuery object
 
   hamburger.addEventListener("click", function () {
     menu.classList.toggle("active-menu");
     header.classList.toggle("active-menu");
     header.classList.toggle("fixed");
-    body.classList.toggle("overflow");
+    body.toggleClass("overflow"); // Utilisation correcte avec jQuery
   });
 });
