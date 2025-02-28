f74455c3245b9ebc5c84c6ba4be6db5a031205f5
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-02-28 11:01:20 +0100

    body hoverflow hidden

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -2,10 +2,12 @@ $(document).ready(function () {
   var hamburger = document.querySelector(".hamburger-menu");
   var menu = document.querySelector(".megamenu");
   var header = document.querySelector(".navigation");
+  var body = document.querySelector("body");
 
   hamburger.addEventListener("click", function () {
     menu.classList.toggle("active-menu");
     header.classList.toggle("active-menu");
     header.classList.toggle("fixed");
+    body.css('overflow','hidden');
   });
 });
