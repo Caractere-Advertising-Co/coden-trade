7115d1fbbc427efbdb55c37c8f767da854d008e2
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-06-16 12:27:37 +0200

    Modification contact.scss

--- templates-parts/contact.php
+++ templates-parts/contact.php
@@ -3,11 +3,18 @@
         <?php 
             $subtitle = get_field('sub_contact','options');
             $title = get_field('titre-contact','options');
+            $titleContact = get_field('titre-page-contact','options');
             $form = get_field('shortcode_form','options');
 
         ?>
         <?php if($subtitle):?><p class="subtitle"><?php echo $subtitle;?></p><?php endif;?>
-        <?php if($title):?> <h2><?php echo $title;?></h2><?php endif;?>
+        
+            
+        <?php if(is_page_template('contact')):
+                if($titleContact): echo $titleContact; endif;
+            else :
+             if($title):?> <h2><?php echo $title;?></h2><?php endif;
+             endif;?>
 
         <?php if($form): echo do_shortcode( $form ); endif;?>
     </div>
